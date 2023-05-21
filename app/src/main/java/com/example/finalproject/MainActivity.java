88966f0569a156c73ca7703a3daaf2a00b90bfcb
faustojc <faustojcboko@gmail.com>
2023-05-21 13:35:20 +0800

    Added reset button, and collision and restart mechanics

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -12,7 +12,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -39,11 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton leftButton;
     private ImageButton rightButton;
+    private Button restartButton;
 
     private boolean leftButtonPressed = false;
     private boolean rightButtonPressed = false;
 
-    private final Timer timer = new Timer();
+    private Timer timer = new Timer();
 
     private final ArrayList<Integer> batoIDs = new ArrayList<>();
 
@@ -71,13 +74,14 @@ public class MainActivity extends AppCompatActivity {
         playerImage = findViewById(R.id.player);
         leftButton = findViewById(R.id.leftButton);
         rightButton = findViewById(R.id.rightButton);
+        restartButton = findViewById(R.id.restartButton);
         initResources();
 
         gameLayoutParams = gameLayout.getLayoutParams();
         groundLayoutParams = ground.getLayoutParams();
 
         // Initialize the player
-        player = new Player(playerImage.getContext(), gameLayoutParams, playerImage);
+        player = new Player(playerImage.getContext(), gameLayoutParams, groundLayoutParams, playerImage);
 
         player.setX(groundLayoutParams.width / 2f);
         player.setY((float) gameLayoutParams.height - (float) groundLayoutParams.height - playerImage.getLayoutParams().height);
@@ -90,53 +94,45 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 // TODO: optimize
 
-                // Move the player
-                runOnUiThread(() -> {
-                    // Drop the stones
-                    for (int i = spawnedStones.size() - 1; i >= 0; i--) {
-                        Stone s = spawnedStones.get(i);
-                        s.moveStone();
-                        s.setRotation(s.getRotation() + s.getSpeed());
-
-                        if ((s.getY() + s.getHeight()) >= (gameLayoutParams.height - groundLayoutParams.height)) {
-                            gameLayout.removeView(s);
-                            spawnedStones.remove(i);
-                        }
-
-                        if (s.getBounds().contains(player.getBounds())) {
-                            Log.d(getClass().getName(), "----- player hit -----");
-
-                            //gameover.setVisibility(View.VISIBLE);
-                            //timer.cancel();
-                            //handler.removeCallbacks(spawnRunnable);
-                            //break;
-                        }
-                    }
-
-                    if (leftButtonPressed || rightButtonPressed) {
-                        if (player.getCurrDirection() == Player.DIRECTION_LEFT) {
-                            playerImage.setX(player.moveLeft());
-                            playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_move_right, null));
-                        }
-                        else if (player.getCurrDirection() == Player.DIRECTION_RIGHT) {
-                            playerImage.setX(player.moveRight());
-                            playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_move_left, null));
-                        }
-                    }
-                    else {
-                        if (player.getPrevDirection() == Player.DIRECTION_LEFT) {
-                            playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_left_idle, null));
-                        }
-                        else if (player.getPrevDirection() == Player.DIRECTION_RIGHT) {
-                            playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_right_idle, null));
-                        }
-                    }
-                });
+                // Run the game in the main UI thread
+                runOnUiThread(() -> runGame());
             }
         }, 0, 20);
 
         buttonMovePlayer();
         handler.post(spawnRunnable);
+
+        // Reset game state
+        restartButton.setOnClickListener(view -> {
+            gameoverImageView.setVisibility(View.INVISIBLE);
+            leftButton.setEnabled(true);
+            rightButton.setEnabled(true);
+            restartButton.setVisibility(View.INVISIBLE);
+
+            for (Stone stone : spawnedStones) {
+                gameLayout.removeView(stone);
+            }
+
+            // Remove all stones
+            spawnedStones.clear();
+
+            // Reset player position and its source ImageView
+            player.resetPosition();
+            playerImage.setX(player.getX());
+            playerImage.setY(player.getY());
+
+            // Start game again
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    // Run the game in the main UI thread
+                    runOnUiThread(() -> runGame());
+                }
+            }, 0, 20);
+
+            handler.post(spawnRunnable);
+        });
     }
 
     @Override
@@ -144,6 +140,58 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         handler.removeCallbacks(spawnRunnable);
         timer.cancel();
+    }
+
+    private void runGame() {
+        // Drop the stones
+        for (int i = spawnedStones.size() - 1; i >= 0; i--) {
+            Stone s = spawnedStones.get(i);
+            s.moveStone();
+            s.setRotation(s.getRotation() + s.getSpeed());
+
+            if ((s.getY() + s.getHeight()) >= (gameLayoutParams.height - groundLayoutParams.height)) {
+                gameLayout.removeView(s);
+                spawnedStones.remove(i);
+            }
+
+            // Check if the stones hit the player
+            if (s.getBounds().intersect(player.getBounds())) {
+                Log.d(getClass().getName(), "----- player hit -----");
+
+                // Stop the game
+                gameoverImageView.setVisibility(View.VISIBLE);
+                restartButton.setVisibility(View.VISIBLE);
+                timer.cancel();
+                handler.removeCallbacks(spawnRunnable);
+                leftButton.setEnabled(false);
+                rightButton.setEnabled(false);
+
+                leftButtonPressed = false;
+                rightButtonPressed = false;
+
+                break;
+            }
+        }
+
+        // Move the player
+        if (leftButtonPressed || rightButtonPressed) {
+            if (player.getCurrDirection() == Player.DIRECTION_LEFT) {
+                playerImage.setX(player.moveLeft());
+                playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_move_right, null));
+            }
+            else if (player.getCurrDirection() == Player.DIRECTION_RIGHT) {
+                playerImage.setX(player.moveRight());
+                playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_move_left, null));
+            }
+        }
+        else {
+            if (player.getPrevDirection() == Player.DIRECTION_LEFT) {
+                playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_left_idle, null));
+            }
+            else if (player.getPrevDirection() == Player.DIRECTION_RIGHT) {
+                playerImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.jarl_right_idle, null));
+            }
+        }
     }
 
     private void initResources() {
